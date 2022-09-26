eb265a0c90d2a84443066d29d07078ffd8ae7dfc
cand0 <rudtjr7655@gmail.com>
2022-09-26 10:23:40 +0000

    simple group_policy test

--- aws/IAM/policy/main.tf
+++ aws/IAM/policy/main.tf
@@ -34,3 +34,45 @@ resource "aws_iam_user_policy" "iam_user_policy_cand1" {
 }
 EOF
 }
+
+resource "aws_iam_group_policy" "iam_group_policy_cand1" {
+  name = "group_policy_cand1"
+  group = aws_iam_group.iam_group_cand1.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:Describe*",
+        ]
+        Effect = "Allow"
+        Resource = "*"
+      }
+    ]
+  }
+  )
+}
+
+resource "aws_iam_policy" "policy_cand1" {
+  name    = "policy_test_cand1"
+  policy  = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "*",
+        ]
+        Effect = "Allow"
+        Resource = "*"
+      }
+    ]
+  }
+  )
+}
+
+resource "aws_iam_group_policy_attachment" "attach_test_cand1" {
+  group = aws_iam_group.iam_group_cand1.name
+  policy_arn = aws_iam_policy.policy_cand1.arn
+}
+
