2dca2a6b6d8ce79ac9842b27e853bcdef72735f7
cand0 <rudtjr7655@gmail.com>
2022-09-30 03:14:02 +0000

    iam_user_login_profile

--- aws/IAM/access_key/main.tf
+++ aws/IAM/access_key/main.tf
@@ -5,3 +5,15 @@ resource "aws_iam_user" "cand1" {
 resource "aws_iam_access_key" "accesskey_cand1" {
   user = aws_iam_user.cand1.name
 }
+
+resource "aws_iam_user" "cand2" {
+  name = "user_cand2"
+}
+
+resource "aws_iam_user_login_profile" "profile_cand2" {
+  user = aws_iam_user.cand2.name
+}
+
+output "password" {
+  value = aws_iam_user_login_profile.profile_cand2.encrypted_password
+}
