b60f8c1bf6569b875ce1993e9240421831a01328
Ubuntu <rudtjr7655@gmail.com>
2022-10-08 04:58:45 +0000

    sg1_sg2 communication rule create

--- aws/EC2/test/cand1/cand1.tf
+++ aws/EC2/test/cand1/cand1.tf
@@ -6,11 +6,32 @@ module "network" {
     subnet_cidr_cand3 = "172.16.30.0/24"
 }
 
-resource "aws_instance" "cand1" {
+resource "aws_instance" "cand1_1" {
   ami           = "ami-08c40ec9ead489470"
   instance_type = "t2.micro"
   subnet_id = module.network.private_subnet_cand1_id
   tags = {
-    Name = "cand1"
+    Name = "cand1_1"
+  }
+  vpc_security_group_ids = [aws_security_group.sg_cand1_1.id]
+}
+
+resource "aws_instance" "cand1_2" {
+  ami           = "ami-08c40ec9ead489470"
+  instance_type = "t2.micro"
+  subnet_id = module.network.private_subnet_cand2_id
+  tags = {
+    Name = "cand1_2"
+  }
+  vpc_security_group_ids = [aws_security_group.sg_cand1_2.id]
+
+}
+
+resource "aws_instance" "cand1_3" {
+  ami           = "ami-08c40ec9ead489470"
+  instance_type = "t2.micro"
+  subnet_id = module.network.private_subnet_cand3_id
+  tags = {
+    Name = "cand1_3"
   }
 }
