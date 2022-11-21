30af274374321b65dc9c712286e84d28291c0c11
cand0 <rudtjr7655@gmail.com>
2022-11-21 19:17:58 +0900

    EBS_cand2 commit

---
+++ aws/Rule/EBS/cand2/cand2.tf
@@ -0,0 +1,16 @@
+resource "aws_kms_key" "cand1" {
+  description = "test1"
+}
+
+
+#testcode
+resource "aws_ebs_default_kms_key" "fail1_1" {
+  key_arn = "${aws_kms_key.cand1.arn}"
+}
+resource "aws_ebs_default_kms_key" "fail1_2" {
+  key_arn = "${aws_kms_key.cand1.arn}"
+}
+
+resource "aws_ebs_default_kms_key" "pass" {
+  key_arn = "${aws_kms_key.cand1.arn}"
+}
