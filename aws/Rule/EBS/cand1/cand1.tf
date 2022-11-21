fb57539e991abd533bae731fb155a8ea4c5bdaed
cand0 <rudtjr7655@gmail.com>
2022-11-21 18:39:58 +0900

    EBS_cand1 commit

---
+++ aws/Rule/EBS/cand1/cand1.tf
@@ -0,0 +1,13 @@
+
+#testcode
+resource "aws_ebs_encryption_by_default" "fail1_1" {
+  enabled = true
+}
+
+resource "aws_ebs_encryption_by_default" "fail1_2" {
+  enabled = false
+}
+
+resource "aws_ebs_encryption_by_default" "pass" {
+  enabled = true
+}
