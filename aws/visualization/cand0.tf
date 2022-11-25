70e4d6b0f15e66bdcb86aa627ddba2aaf5313db6
cand0 <rudtjr7655@gmail.com>
2022-11-26 01:29:37 +0900

    visualization commit

--- aws/visualization/cand0.tf
+++ aws/visualization/cand0.tf
@@ -1,14 +1,5 @@
-/*
-resource "aws_instance" "cand2" {
-  ami           = "ami-08c40ec9ead489470"
-  instance_type = "t2.micro"
-  subnet_id = module.network.private_subnet_cand1_id
-  tags = {
-    Name = "cand2"
-  }
-}
-*/
-/*
+
+
 resource "aws_vpc" "cand1" {
   cidr_block = "10.1.0.0/16"
 }
@@ -97,10 +88,9 @@ resource "aws_vpc_endpoint_subnet_association" "cand1" {
   vpc_endpoint_id = aws_vpc_endpoint.cand1_2.id
   subnet_id       = aws_subnet.cand1_cand2.id
 }
-*/
 
 
-/*
+
 resource "aws_subnet" "cand1_cand3" {
   vpc_id = "${aws_vpc.cand1.id}"
   cidr_block = cidrsubnet(aws_vpc.cand1.cidr_block, 8, 3)
@@ -154,14 +144,14 @@ resource "aws_vpc_peering_connection_accepter" "cand1" {
   vpc_peering_connection_id = "${aws_vpc_peering_connection.cand1.id}"
   auto_accept = true
 }
-*/
+
 resource "aws_vpc" "cand2" {
   cidr_block = "10.2.0.0/16"
 }
 resource "aws_route_table" "cand2" {
   vpc_id = aws_vpc.cand2.id
 }
-/*
+
 # vpc peering connect
 resource "aws_route" "cand1" {
   route_table_id = "${aws_route_table.cand2.id}"
@@ -181,9 +171,10 @@ resource "aws_subnet" "cand2_cand2" {
   vpc_id = "${aws_vpc.cand2.id}"
   cidr_block = cidrsubnet(aws_vpc.cand2.cidr_block, 8, 2)
 }
-resource "aws_internet_gateway" "cand1" {}
-resource "aws_internet_gateway_attachment" "cand1" {
-  internet_gateway_id = "${aws_internet_gateway.cand1.id}"
+
+resource "aws_internet_gateway" "cand2_1" {}
+resource "aws_internet_gateway_attachment" "cand2_1" {
+  internet_gateway_id = "${aws_internet_gateway.cand2_1.id}"
   vpc_id              = "${aws_vpc.cand2.id}"
 }
 resource "aws_eip" "cand1" {
@@ -212,7 +203,7 @@ resource "aws_route_table_association" "cand2_2_1_natgatewaytest" {
   subnet_id = "${aws_subnet.cand2_cand3.id}"
 }
 
-*/
+
 
 resource "aws_subnet" "cand2_cand4" {
   vpc_id = "${aws_vpc.cand2.id}"
@@ -254,7 +245,10 @@ resource "aws_instance" "cand2_cand4_cand1" {
 
 
 
-/*
+
+
+
+#efs test
 resource "aws_vpc" "cand3" {
   cidr_block = "10.3.0.0/16"
 }
@@ -262,17 +256,19 @@ resource "aws_subnet" "cand3_cand1" {
   vpc_id = "${aws_vpc.cand3.id}"
   cidr_block = cidrsubnet(aws_vpc.cand3.cidr_block, 8, 1)
 }
+resource "aws_efs_file_system" "cand1" {
+  creation_token = "test"
+}
+resource "aws_efs_mount_target" "cand1" {
+  file_system_id = "${aws_efs_file_system.cand1.id}"
+  subnet_id      = "${aws_subnet.cand3_cand1.id}"
+}
+
+
+
 resource "aws_subnet" "cand3_cand2" {
   vpc_id = "${aws_vpc.cand3.id}"
   cidr_block = cidrsubnet(aws_vpc.cand3.cidr_block, 8, 2)
-}
-resource "aws_subnet" "cand3_cand3" {
-  vpc_id = "${aws_vpc.cand3.id}"
-  cidr_block = cidrsubnet(aws_vpc.cand3.cidr_block, 8, 3)
-}
-resource "aws_subnet" "cand3_cand4" {
-  vpc_id = "${aws_vpc.cand3.id}"
-  cidr_block = cidrsubnet(aws_vpc.cand3.cidr_block, 8, 4)
 }
 
 resource "aws_vpc" "cand4" {
@@ -294,5 +290,7 @@ resource "aws_subnet" "cand4_cand4" {
   vpc_id = "${aws_vpc.cand4.id}"
   cidr_block = cidrsubnet(aws_vpc.cand4.cidr_block, 8, 4)
 }
-
-*/
+resource "aws_subnet" "cand4_cand5" {
+  vpc_id = "${aws_vpc.cand4.id}"
+  cidr_block = cidrsubnet(aws_vpc.cand4.cidr_block, 8, 5)
+}
